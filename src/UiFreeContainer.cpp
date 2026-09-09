cce4c1fa0731f47fff5bcb4acc241c57284a82c0
Óscar Pozo <oskiyu@gmail.com>
2026-09-09 18:58:21 +0200

    Fix: FreeContainer now takes into account its own padding when placing child elements

--- src/UiFreeContainer.cpp
+++ src/UiFreeContainer.cpp
@@ -1,5 +1,8 @@
 #include "UiFreeContainer.h"
 
+#include "OSKengine.h"
+#include "Logger.h"
+
 using namespace OSK;
 using namespace OSK::UI;
 
@@ -8,33 +11,27 @@ FreeContainer::FreeContainer(const Vector2f& size) : IContainer(size) {
 }
 
 void FreeContainer::EmplaceChild(IElement* child) {
-	Vector2f childOffset = Vector2f::Zero;
+	using EFTraits::HasFlag;
+	using enum Anchor;
+
+	auto childOffset = Vector2f::Zero;
+
+	const auto parentSize  = this ->GetSize();
+	const auto childAnchor = child->GetAnchor();
+	const auto childMargin = child->GetMarging();
+	const auto childSize   = child->GetSize();
 
 	// Eje X
-	if (EFTraits::HasFlag(child->GetAnchor(), Anchor::LEFT)) {
-		childOffset.x = 0.0f + child->GetMarging().x;
-	}
-	else if (EFTraits::HasFlag(child->GetAnchor(), Anchor::CENTER_X)) {
-		childOffset.x = GetSize().x * 0.5f - child->GetSize().x * 0.5f;
-	}
-	else if (EFTraits::HasFlag(child->GetAnchor(), Anchor::RIGHT)) {
-		childOffset.x = GetSize().x - child->GetSize().x - child->GetMarging().z;
-	}
-
+	if (HasFlag(childAnchor, LEFT))     childOffset.x = parentSize.x * 0.0f - childSize.x * 0.0f + childMargin.x + GetPadding().x;
+	if (HasFlag(childAnchor, CENTER_X)) childOffset.x = parentSize.x * 0.5f - childSize.x * 0.5f;
+	if (HasFlag(childAnchor, RIGHT))    childOffset.x = parentSize.x * 1.0f - childSize.x * 1.0f - childMargin.z - GetPadding().z;
+	
 	// Eje Y
-	if (EFTraits::HasFlag(child->GetAnchor(), Anchor::TOP)) {
-		childOffset.y = 0.0f + child->GetMarging().y;
-	}
-	else if (EFTraits::HasFlag(child->GetAnchor(), Anchor::CENTER_Y)) {
-		childOffset.y = GetSize().y * 0.5f - child->GetSize().y * 0.5f;
-	} 
-	else if (EFTraits::HasFlag(child->GetAnchor(), Anchor::BOTTOM)) {
-		childOffset.y = GetSize().y - child->GetSize().y - child->GetMarging().z;
-	}
+	if (HasFlag(childAnchor, TOP     )) childOffset.y = parentSize.y * 0.0f - childSize.y * 0.0f + childMargin.y + GetPadding().y;
+	if (HasFlag(childAnchor, CENTER_Y)) childOffset.y = parentSize.y * 0.5f - childSize.y * 0.5f;
+	if (HasFlag(childAnchor, BOTTOM  )) childOffset.y = parentSize.y * 1.0f - childSize.y * 1.0f - childMargin.w - GetPadding().w;
 
-	const Vector2f totalOffset = childOffset;
-
-	child->_SetPosition(GetPosition() + totalOffset);
+	child->_SetPosition(GetPosition() + childOffset);
 }
 
 void FreeContainer::ResetLayout() {
